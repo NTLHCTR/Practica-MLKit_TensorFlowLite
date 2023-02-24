a074f86749caeff8c18aed3cf4abafa50e6b6235
Hector Steeven Paladines Cueva <hector.paladines2018@uteq.edu.ec>
2023-02-23 23:51:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/practicamachinelearningkit/MainActivity.java
@@ -0,0 +1,341 @@
+package com.example.practicamachinelearningkit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.objects.ObjectDetection;
+import com.google.mlkit.vision.objects.ObjectDetector;
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity
+        implements OnSuccessListener<Text>, OnFailureListener, ImageAnalysis.Analyzer
+{
+
+    public static int REQUEST_CAMERA = 111;
+    public static int REQUEST_GALLERY = 222;
+    public Bitmap mSelectedImage;
+    public ImageView mImageView;
+    public TextView txtResults;
+
+    public Button btCamera, btGaleria;
+    public ArrayList<String> permisosNoAprobados;
+
+    Vision vision;
+    public int cont = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.image_view);
+        txtResults = findViewById(R.id.txtresults);
+        mImageView = findViewById(R.id.image_view);
+        btCamera = findViewById(R.id.btCamera);
+        btGaleria = findViewById(R.id.btGallery);
+
+        /*ArrayList<String> permisos_requeridos = new ArrayList<String>();
+        permisos_requeridos.add(Manifest.permission.CAMERA);
+        permisos_requeridos.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE);
+        permisos_requeridos.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        permisosNoAprobados  = getPermisosNoAprobados(permisos_requeridos);
+        requestPermissions(permisosNoAprobados.toArray(new String[permisosNoAprobados.size()]), 100);*/
+
+        Vision.Builder visionBuilder = new Vision.Builder(new NetHttpTransport(),
+                new AndroidJsonFactory(),  null);
+
+        visionBuilder.setVisionRequestInitializer(new VisionRequestInitializer("AIzaSyB5MkIB5lNnQH1kC1tZ3ATeEsv7z66moKs"));
+        vision = visionBuilder.build();
+    }
+
+    /*public ArrayList<String> getPermisosNoAprobados(ArrayList<String>  listaPermisos) {
+        ArrayList<String> list = new ArrayList<String>();
+        Boolean habilitado;
+
+        if (Build.VERSION.SDK_INT >= 23)
+            for(String permiso: listaPermisos) {
+                if (checkSelfPermission(permiso) != PackageManager.PERMISSION_GRANTED) {
+                    list.add(permiso);
+                    habilitado = false;
+                }else
+                    habilitado=true;
+
+                if(permiso.equals(Manifest.permission.CAMERA))
+                    btCamera.setEnabled(habilitado);
+                else if (permiso.equals(Manifest.permission.MANAGE_EXTERNAL_STORAGE)  ||
+                        permiso.equals(Manifest.permission.READ_EXTERNAL_STORAGE))
+                    btGaleria.setEnabled(habilitado);
+            }
+        return list;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int i=0; i<permissions.length; i++){
+            if(permissions[i].equals(Manifest.permission.CAMERA)){
+                btCamera.setEnabled(grantResults[i] == PackageManager.PERMISSION_GRANTED);
+            } else if(permissions[i].equals(Manifest.permission.MANAGE_EXTERNAL_STORAGE) ||
+                    permissions[i].equals(Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                    permissions[i].equals(Manifest.permission.READ_EXTERNAL_STORAGE)
+            ) {
+                btGaleria.setEnabled(grantResults[i] == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+    }*/
+
+    public void abrirGaleria (View view){
+    Intent i = new Intent(Intent.ACTION_PICK,
+            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+    startActivityForResult(i, REQUEST_GALLERY);
+    }
+
+    public void abrirCamara (View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode == RESULT_OK && null != data) {
+        try {
+            if (requestCode == REQUEST_CAMERA)
+                mSelectedImage = (Bitmap) data.getExtras().get("data");
+            else
+                 mSelectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+
+            mImageView.setImageBitmap(mSelectedImage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void OCRfx(View v) {
+    InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
+    TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+    recognizer.process(image)
+            .addOnSuccessListener(this)
+            .addOnFailureListener(this);
+    }
+
+    public void Rostrosfx(View  v) {
+    InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
+    FaceDetectorOptions options = new FaceDetectorOptions.Builder()
+                    .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                    .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+                    .build();
+
+    FaceDetector detector = FaceDetection.getClient(options);
+    detector.process(image)
+            .addOnSuccessListener(new OnSuccessListener<List<Face>>() {
+                @Override
+                public void onSuccess(List<Face> faces) {
+                    if (faces.size() == 0) {
+                        txtResults.setText("No Hay rostros");
+                    }else{
+                        txtResults.setText("Hay " + faces.size() + " rostro(s)");
+                    }
+                }
+            })
+            .addOnFailureListener(this);
+}
+
+
+
+    /*@Override
+    public void onFailure(@NonNull Exception e) {
+        
+    }
+
+    @Override
+    public void onSuccess(Text text) {
+        List<Text.TextBlock> blocks = text.getTextBlocks();
+    String resultados="";
+    if (blocks.size() == 0) {
+        resultados = "No hay Texto";
+    }
+    else{
+        for (int i = 0; i < blocks.size(); i++) {
+           List<Text.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                 List<Text.Element> elements = lines.get(j).getElements();
+                 for (int k = 0; k < elements.size(); k++) {
+                     resultados = resultados + elements.get(k).getText() + " ";
+                 }
+            }
+            resultados=resultados + "\n";
+        }
+    }
+    txtResults.setText(resultados);
+
+    }
+
+    //Objetos
+    ObjectDetectorOptions options =
+            new ObjectDetectorOptions.Builder()
+                    .setDetectorMode(ObjectDetectorOptions.SINGLE_IMAGE_MODE)
+                    .enableMultipleObjects()
+                    .enableClassification()  // Optional
+                    .build();
+
+    ObjectDetector objectDetector = ObjectDetection.getClient(options);
+
+    @Override
+    public void analyze(@NonNull View v) {
+        InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
+        ObjectDetectorOptions options = new ObjectDetectorOptions()
+                .getDetectorMode(ObjectDetectorOptions.DEFAULT_OPTIONS);
+    }*/
+
+    public BatchAnnotateImagesRequest setBatchRequest(String TipoSolic, Image inputImage){
+
+        Feature desiredFeature = new Feature();
+        desiredFeature.setType(TipoSolic);
+
+        AnnotateImageRequest request = new AnnotateImageRequest();
+        request.setImage(inputImage);
+        request.setFeatures(Arrays.asList(desiredFeature));
+
+
+        BatchAnnotateImagesRequest batchRequest =  new BatchAnnotateImagesRequest();
+        batchRequest.setRequests(Arrays.asList(request));
+        return batchRequest;
+    }
+
+    private Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+
+    public Image getImageToProcess(){
+
+        ImageView imagen = (ImageView)findViewById(R.id.image_view);
+        BitmapDrawable drawable = (BitmapDrawable) imagen.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        bitmap = scaleBitmapDown(bitmap, 800);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, stream);
+        byte[] imageInByte = stream.toByteArray();
+
+        Image inputImage = new Image();
+        inputImage.encodeContent(imageInByte);
+
+        return inputImage;
+    }
+
+    public void ProcesarObjetos(View View){
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                BatchAnnotateImagesRequest batchRequest = setBatchRequest("LABEL_DETECTION", getImageToProcess());
+
+                try {
+                    Vision.Images.Annotate  annotateRequest = vision.images().annotate(batchRequest);
+                    annotateRequest.setDisableGZipContent(true);
+                    BatchAnnotateImagesResponse response  = annotateRequest.execute();
+
+                    final StringBuilder message = new StringBuilder("Se ha encontrado los siguientes Objetos:\n\n");
+
+                    List<EntityAnnotation> labels = response.getResponses().get(0).getLabelAnnotations();
+                    if (labels != null) {
+                        for (EntityAnnotation label : labels)
+                            message.append(String.format( Locale.US, "- Confianza: %.2f - Texto: %s\n", label.getScore()*100, label.getDescription()));
+
+                    } else {
+                        message.append("No hay ningún Objeto");
+                    }
+
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView imageDetail = (TextView)findViewById(R.id.txtresults);
+                            imageDetail.setText(message.toString());
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //cont =cont + 1; - Etiqueta: %s
+            }
+        });
+    }
+
+    @Override
+    public void analyze(@NonNull ImageProxy image) {
+
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+
+    }
+
+    @Override
+    public void onSuccess(Text text) {
+
+    }
+}
